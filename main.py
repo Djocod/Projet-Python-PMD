774169a2fe50d2f406d66d170244e950712b2885
Djocod <jordan.francoual@hotmail.com>
2025-11-23 15:01:15 +0100

    error

--- main.py
+++ main.py
@@ -1,47 +1,11 @@
-def main_menu():
-  '''
-  affiche le menu principal du jeu
-  '''
-#commencer le jeu : active la fonction introduction
-#changer la langue +5
-#presentation de colloborateur = credit
-#quit game
+from function import *
+# from data_game import Player, Map
 
-pass
-
-def about ():
-  pass
-
-#def language(): +5 si on a le temps on le fait
-
-
-def quit_game(): #dans le menu pour quitter le jeu
-  print("Voulez-vous quitter la partie? oui ou non?")
-  user_choice = input().lower()
-  if user_choice == "oui":
-    return # soit return de la boucle de jeu // soit appeler le menu ? --> return start dans le jeu, et quit dans le menu
-
-
-def introduction():
-
-''' #if choise is yes = lancement tuto sinon game start question pour le end_user
-(input)
-lance un combat vide --> function fight se lance
- '''
-
-  print("Est-ce que vous souhaitez voir le tuto?")
-
-  #festival name (print)
-  #name of the player (input)--> dico player
-
-  #presentation du jeu (print)
-  #fonction tuto combat (fonction fight_enemy)
-  #return fin introduction
-  #appeller la fonction start_game
-
-  pass
-
-def start_game():
+def start_game(): 
+  current_position = Map['B1']
+  move( Player, current_position)
+  return
+ 
     
     
   #le debut du jeu --> entree en B1
@@ -54,5 +18,4 @@ def start_game():
 #   player ne decide pas de quitter le jeu --> le jeu continue
 #   '''
 
-
-  pass
+start_game()
